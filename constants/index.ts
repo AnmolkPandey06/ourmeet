f429ce2cddb168d1cf729d1f75729d0ff89c976b
AnmolkPandey06 <anmolkp0612@gmail.com>
2024-04-04 23:30:05 +0530

    initial setups and sidebars

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const SideBarlinks=[
+    {
+        imgUrl:'/icons/Home.svg',
+        route:'/',
+        label:'Home'
+    },
+    {
+        imgUrl:'/icons/upcoming.svg',
+        route:'/upcoming',
+        label:'Upcoming Meetings'
+    },
+    {
+        imgUrl:'/icons/previous.svg',
+        route:'/previous',
+        label:'Previous Meetings'
+    },
+    {
+        imgUrl:'/icons/Video.svg',
+        route:'/recordings',
+        label:'Recordings'
+    },
+    {
+        imgUrl:'/icons/add-personal.svg',
+        route:'/personal-room',
+        label:'Personal Room'
+    },
+]
